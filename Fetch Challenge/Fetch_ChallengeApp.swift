16bdbd141d648d402af362777917f44035a3e9e3
wytan01 <weiyitan2001@gmail.com>
2024-02-20 14:04:24 -0500

    Initial Commit

---
+++ Fetch Challenge/Fetch_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ChallengeApp.swift
+//  Fetch Challenge
+//
+//  Created by Weiyi Tan on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
